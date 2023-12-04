59d1f457e720552197e5e4c09ca653897fe22756
NeeZoxe <113175291+NeeZoxe@users.noreply.github.com>
2023-12-04 15:45:57 +0100

    Modification js ajout d'une boucle

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
+// api key
 const APIKEY = "850c786aef4568027bcaa5773c77ba60";
 
+// fonction de chargement des données météo sur le fichier html 
 let apiCall = function (city) {
-    
+
+// url api key
 let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric&lang=fr
 `;
 
@@ -21,6 +24,19 @@ let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKE
         .catch((err) => console.log('Erreur : ' + err));
 };
 
+// Fonction pour mettre à jour les données toutes les minutes
+let updateData = function () {
+    let ville = document.querySelector('#inputCity').value;
+    apiCall(ville);
+};
+
+// Appel initial de la fonction
+updateData();
+
+// Mise à jour toutes les minutes (60 000 millisecondes)
+setInterval(updateData, 3600000);
+
+// écouteur d'événement sur le bouton de recherche
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     let ville = document.querySelector('#inputCity').value;
@@ -28,4 +44,5 @@ document.querySelector('form').addEventListener('submit', function (e) {
     apiCall(ville);
 });
 
+// appelle par défaut au chargement de la page
 apiCall('Niort');
